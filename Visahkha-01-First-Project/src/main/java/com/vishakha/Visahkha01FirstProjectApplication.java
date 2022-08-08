51864722ab5b236c749ba03a8387ed4196b766be
VishakhaBairamwar <bairamwarvishakha@gmail.com>
2022-08-08 23:55:26 +0530

    vishakha first project added

---
+++ Visahkha-01-First-Project/src/main/java/com/vishakha/Visahkha01FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vishakha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Visahkha01FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Visahkha01FirstProjectApplication.class, args);
+	}
+
+}
---
+++ Visahkha-01-First-Project/src/test/java/com/vishakha/Visahkha01FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishakha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Visahkha01FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
